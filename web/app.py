22979306944d129ed3b7804066283f996bdb9ed9
thato <thato.seboko@gmail.com>
2019-11-06 15:01:32 +0200

    Added default config

--- web/app.py
+++ web/app.py
@@ -8,9 +8,7 @@ from resources.user import UserResource, UserRegisterResource
 from resources.case import CaseResource, CasesResource
 
 app = Flask(__name__)
-app.config['MONGOALCHEMY_DATABASE'] = 'mymongodb'
-app.config['MONGOALCHEMY_SERVER'] = 'db'
-app.config['MONGOALCHEMY_PORT'] = '27017'
+app.config.from_object("default_config")
 api = Api(app)
 
 api.add_resource(UserResource, '/api/user/<string:username>')
